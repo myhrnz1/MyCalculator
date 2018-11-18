b4da061f1bbdf59f81780e7a1af279d02331e1f9
myhrnz1 <myhrnz@outlook.com>
2018-11-18 10:24:03 +0100

    Add addition

--- MyCalculator.java
+++ MyCalculator.java
@@ -10,6 +10,16 @@ public class MyCalculator {
     System.out.println("4) Division");
     System.out.println("Choose operation: ");
   }
+
+  public double add(Scanner scan) {
+    System.out.print("First number: ");
+    double operand1 = scan.nextDouble();
+    System.out.print("Second number: ");
+    double operand2 = scan.nextDouble();
+
+    double result = operand1 + operand2;
+    return result;
+  }
   
   public static void main (String[] args) {
 
@@ -22,16 +32,17 @@ public class MyCalculator {
   int operation = scan.nextInt();
 
   switch(operation) {
-	case 1 : System.out.println("You chose add");
-		 break;
-	case 2 : System.out.println("You chose sub");
-		 break;
-	case 3 : System.out.println("You chose mult");
-		 break;
-	case 4 : System.out.println("You chose div");
-		 break;
-	default: System.out.println("Not a valid operation!");
-		 break;
+    case 1 : result = calc.add(scan);
+      break;
+    case 2 : System.out.println("You chose sub");
+      break;
+    case 3 : System.out.println("You chose mult");
+      break;
+    case 4 : System.out.println("You chose div");
+      break;
+    default: System.out.println("Not a valid operation!");
+      break;
     }
+  System.out.println("Result: "+ result);
   }
 }
